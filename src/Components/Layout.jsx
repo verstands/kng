950c54ff1897ad25a1d32155c2e98ba4a7fdcb1b
jnthnkabongo <jnthnkabongo@gmail.com>
2024-05-19 22:46:31 +0100

    rien

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -7,12 +7,12 @@ const Layout = () => {
     profileData = JSON.parse(profileData);
     return (
         <>
-            <div class="layout-wrapper layout-content-navbar">
-                <div class="layout-container">
-                    <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
-                        <div class="app-brand demo">
-                            <a href="index.html" class="app-brand-link">
-                                <span class="app-brand-logo demo">
+            <div className="layout-wrapper layout-content-navbar">
+                <div className="layout-container">
+                    <aside id="layout-menu" className="layout-menu menu-vertical menu bg-menu-theme">
+                        <div className="app-brand demo">
+                            <a href="index.html" className="app-brand-link">
+                                <span className="app-brand-logo demo">
                                     <svg
                                         width="25"
                                         viewBox="0 0 25 42"
@@ -38,7 +38,7 @@ const Layout = () => {
                                                 id="path-5"
                                             ></path>
                                         </defs>
-                                        <g id="g-app-brand" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                        <g id="g-app-brand" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
                                             <g id="Brand-Logo" transform="translate(-27.000000, -15.000000)">
                                                 <g id="Icon" transform="translate(27.000000, 15.000000)">
                                                     <g id="Mask" transform="translate(0.000000, 8.000000)">
@@ -48,11 +48,11 @@ const Layout = () => {
                                                         <use fill="#696cff" xlink: href="#path-1"></use>
                                                         <g id="Path-3" mask="url(#mask-2)">
                                                             <use fill="#696cff" xlink: href="#path-3"></use>
-                                                            <use fill-opacity="0.2" fill="#FFFFFF" xlink: href="#path-3"></use>
+                                                            <use fillOpacity="0.2" fill="#FFFFFF" xlink: href="#path-3"></use>
                                                         </g>
                                                         <g id="Path-4" mask="url(#mask-2)">
                                                             <use fill="#696cff" xlink: href="#path-4"></use>
-                                                            <use fill-opacity="0.2" fill="#FFFFFF" xlink: href="#path-4"></use>
+                                                            <use fillOpacity="0.2" fill="#FFFFFF" xlink: href="#path-4"></use>
                                                         </g>
                                                     </g>
                                                     <g
@@ -60,101 +60,101 @@ const Layout = () => {
                                                         transform="translate(19.000000, 11.000000) rotate(-300.000000) translate(-19.000000, -11.000000) "
                                                     >
                                                         <use fill="#696cff" xlink: href="#path-5"></use>
-                                                        <use fill-opacity="0.2" fill="#FFFFFF" xlink: href="#path-5"></use>
+                                                        <use fillOpacity="0.2" fill="#FFFFFF" xlink: href="#path-5"></use>
                                                     </g>
                                                 </g>
                                             </g>
                                         </g>
                                     </svg>
                                 </span>
-                                <span class="app-brand-text demo menu-text fw-bolder ms-2">Sneat</span>
+                                <span className="app-brand-text demo menu-text fw-bolder ms-2">Sneat</span>
                             </a>
-                            <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
-                                <i class="bx bx-chevron-left bx-sm align-middle"></i>
+                            <a href="javascript:void(0);" className="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+                                <i className="bx bx-chevron-left bx-sm align-middle"></i>
                             </a>
                         </div>
-                        <div class="menu-inner-shadow"></div>
-                        <ul class="menu-inner py-1">
+                        <div className="menu-inner-shadow"></div>
+                        <ul className="menu-inner py-1">
                             <li className={`menu-item ${location.pathname === '/dashboad' ? 'active' : ''}`}>
                                 <Link to="/dashboad" class="menu-link">
-                                    <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                                    <i className="menu-icon tf-icons bx bx-home-circle"></i>
                                     <div data-i18n="Analytics">Dashboard</div>
                                 </Link>
                             </li>
                             <li className={`menu-item ${location.pathname === '/entrer' ? 'active' : ''}`}>
                                 <Link to="/entrer" class="menu-link">
-                                    <i class="menu-icon tf-icons bx bx-log-in"></i>
+                                    <i className="menu-icon tf-icons bx bx-log-in"></i>
                                     <div data-i18n="Analytics">DXB ABS</div>
                                 </Link>
                             </li>
                             <li className={`menu-item ${location.pathname === '/sortir' ? 'active' : ''}`}>
                                 <Link to="/sortir" class="menu-link">
-                                    <i class="menu-icon tf-icons bx bx-log-out"></i>
+                                    <i className="menu-icon tf-icons bx bx-log-out"></i>
                                     <div data-i18n="Analytics">FIH ABS</div>
                                 </Link>
                             </li>
                             <li className={`menu-item ${location.pathname === '/detteClient' ? 'active' : ''}`}>
                                 <Link to="/detteClient" class="menu-link">
-                                    <i class="menu-icon tf-icons bx bx-wallet"></i>
+                                    <i className="menu-icon tf-icons bx bx-wallet"></i>
                                     <div data-i18n="Analytics">Dette</div>
                                 </Link>
                             </li>
                             <li className={`menu-item ${location.pathname === '/conteneur' ? 'active' : ''}`}>
                                 <Link to="/conteneur" class="menu-link">
-                                    <i class="menu-icon tf-icons bx bx-archive"></i>
+                                    <i className="menu-icon tf-icons bx bx-archive"></i>
                                     <div data-i18n="Analytics">Conteneur</div>
                                 </Link>
                             </li>
                             
                             <li className={`menu-item ${location.pathname === '/compte' ? 'active' : ''}`}>
                                 <Link to="/compte" class="menu-link">
-                                    <i class="menu-icon tf-icons bx bx-user-circle"></i>
+                                    <i className="menu-icon tf-icons bx bx-user-circle"></i>
                                     <div data-i18n="Analytics">Mon compte</div>
                                 </Link>
                             </li>
                             <li className={`menu-item ${location.pathname === '/cloture' ? 'active' : ''}`}>
                                 <Link to="/cloture" class="menu-link">
-                                    <i class="menu-icon tf-icons bx bx-x"></i>
+                                    <i className="menu-icon tf-icons bx bx-x"></i>
                                     <div data-i18n="Analytics">Cloture</div>
                                 </Link>
                             </li>
                             <li className={`menu-item ${location.pathname === '/Parametre' ? 'active' : ''}`}>
                                 <Link to="/Parametre" class="menu-link">
-                                    <i class="menu-icon tf-icons bx bx-cog"></i>
+                                    <i className="menu-icon tf-icons bx bx-cog"></i>
                                     <div data-i18n="Analytics">Parametre</div>
                                 </Link>
                             </li>
                         </ul>
                     </aside>
-                    <div class="layout-page">
+                    <div className="layout-page">
 
                         <nav
-                            class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+                            className="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
                             id="layout-navbar"
                         >
-                            <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
-                                <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
-                                    <i class="bx bx-menu bx-sm"></i>
+                            <div className="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+                                <a className="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+                                    <i className="bx bx-menu bx-sm"></i>
                                 </a>
                             </div>
 
-                            <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
-                                <div class="navbar-nav align-items-center">
-                                    <div class="nav-item d-flex align-items-center">
-                                        <i class="bx bx-search fs-4 lh-0"></i>
+                            <div className="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+                                <div className="navbar-nav align-items-center">
+                                    <div className="nav-item d-flex align-items-center">
+                                        <i className="bx bx-search fs-4 lh-0"></i>
                                         <input
                                             type="text"
-                                            class="form-control border-0 shadow-none"
+                                            className="form-control border-0 shadow-none"
                                             placeholder="Search..."
                                             aria-label="Search..."
                                         />
                                     </div>
                                 </div>
 
-                                <ul class="navbar-nav flex-row align-items-center ms-auto">
-                                    <li class="nav-item lh-1 me-3">
+                                <ul className="navbar-nav flex-row align-items-center ms-auto">
+                                    <li className="nav-item lh-1 me-3">
                                         <a
-                                            class="github-button"
+                                            className="github-button"
                                             href="https://github.com/themeselection/sneat-html-admin-template-free"
                                             data-icon="octicon-star"
                                             data-size="large"
@@ -164,45 +164,45 @@ const Layout = () => {
                                         >
                                     </li>
 
-                                    <li class="nav-item navbar-dropdown dropdown-user dropdown">
-                                        <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
-                                            <div class="avatar avatar-online">
-                                                <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                                    <li className="nav-item navbar-dropdown dropdown-user dropdown">
+                                        <a className="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+                                            <div className="avatar avatar-online">
+                                                <img src="../assets/img/avatars/1.png" alt className="w-px-40 h-auto rounded-circle" />
                                             </div>
                                         </a>
-                                        <ul class="dropdown-menu dropdown-menu-end">
+                                        <ul className="dropdown-menu dropdown-menu-end">
                                             <li>
-                                                <a class="dropdown-item" href="#">
-                                                    <div class="d-flex">
-                                                        <div class="flex-shrink-0 me-3">
-                                                            <div class="avatar avatar-online">
-                                                                <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                                                <a className="dropdown-item" href="#">
+                                                    <div className="d-flex">
+                                                        <div className="flex-shrink-0 me-3">
+                                                            <div className="avatar avatar-online">
+                                                                <img src="../assets/img/avatars/1.png" alt className="w-px-40 h-auto rounded-circle" />
                                                             </div>
                                                         </div>
-                                                        <div class="flex-grow-1">
-                                                            <span class="fw-semibold d-block">{profileData.nom} {profileData.postnom}</span>
-                                                            <small class="text-muted">Admin</small>
+                                                        <div className="flex-grow-1">
+                                                            <span className="fw-semibold d-block">{profileData.nom} {profileData.postnom}</span>
+                                                            <small className="text-muted">Admin</small>
                                                         </div>
                                                     </div>
                                                 </a>
                                             </li>
                                             <li>
-                                                <div class="dropdown-divider"></div>
+                                                <div className="dropdown-divider"></div>
                                             </li>
                                             <li>
-                                                <a class="dropdown-item" href="#">
-                                                    <i class="bx bx-user me-2"></i>
-                                                    <span class="align-middle">Mon profile</span>
+                                                <a className="dropdown-item" href="#">
+                                                    <i className="bx bx-user me-2"></i>
+                                                    <span className="align-middle">Mon profile</span>
                                                 </a>
                                             </li>
                                             
                                             <li>
-                                                <div class="dropdown-divider"></div>
+                                                <div className="dropdown-divider"></div>
                                             </li>
                                             <li>
                                                 <Link to="/deconnextion" class="dropdown-item" href="auth-login-basic.html">
-                                                    <i class="bx bx-power-off me-2"></i>
-                                                    <span class="align-middle">Se deconnecter</span>
+                                                    <i className="bx bx-power-off me-2"></i>
+                                                    <span className="align-middle">Se deconnecter</span>
                                                 </Link>
                                             </li>
                                         </ul>
@@ -210,7 +210,7 @@ const Layout = () => {
                                 </ul>
                             </div>
                         </nav>
-                        <div class="content-wrapper">
+                        <div className="content-wrapper">
                             <Outlet />
                         </div>
 
